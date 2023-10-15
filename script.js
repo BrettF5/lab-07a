97c2f96aba07c47388b1f3d32ab9f66eaf71c460
Brett Fort <147459646+BrettF5@users.noreply.github.com>
2023-10-14 20:51:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ function locationCheck(userCity){
     if(userCity === ''){
         locationPlace = prompt('Please tell me where your from if you want to');
     } else {
-        let userFromCity = locationPlace}
+        let userCity = locationPlace}
 
         return locationPlace;
     }
 
-// function whatLocation(message){
-//     if(message === 'Seattle'){
-//         document.write("Welcome Seattle Mariner Fan!!!");
-//     } else if (message === 'New York'){
-//         document.write('Welcome New York Yankee Fan!!!');
-//     } else if (message === 'Boston'){
-//         document.write('Welcome Boston Red Sox Fan!!! Sorry for a bad season...') ;    
-//     } else {
-//         document.write('Sorry, do not have a message for your location. Please enter Seattle, New York or Boston for a different resoonse.');
-//     }
+function whatLocation(message){
+    if(message === 'Seattle'){
+        document.write("Welcome Seattle Mariner Fan!!!");
+    } else if (message === 'New York'){
+        document.write('Welcome New York Yankee Fan!!!');
+    } else if (message === 'Boston'){
+        document.write('Welcome Boston Red Sox Fan!!! Sorry for a bad season...') ;    
+    } else {
+        document.write('Sorry, do not have a message for your location. Please enter Seattle, New York or Boston for a different resoonse.');
+    }
     
-//  }
+ }
 
